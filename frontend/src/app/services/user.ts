da73fd20d006a8e77b93fea7f5fd58b2ec69bd5c
Rehan-Pathan <rehanpathanofficial@gmail.com>
2026-03-15 14:34:14 +0530

    user-service

--- frontend/src/app/services/user.ts
+++ frontend/src/app/services/user.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class User {
+export class User { 
 
   private baseUrl = 'http://localhost:8080/users';
 
